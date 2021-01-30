310989ecebd4f7a6f1b31772922b5be1a4fa73d7
Adrian <adrian.lu95@gmail.com>
2021-01-29 16:03:08 -0800

    added router handling for all user endpoints

--- router/router.go
+++ router/router.go
@@ -1,1 +1,22 @@
 package router
+
+import (
+	"go-postgres-practice/middleware"
+
+	"github.com/gorilla/mux"
+)
+
+// Router is exported and used in main.go to define and handle all endpoints and their respective middleware
+func Router() *mux.Router {
+	var router *mux.Router
+
+	router = mux.NewRouter()
+
+	router.HandleFunc("/api/user/{id}", middleware.GetUser).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/user", middleware.GetAllUser).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/newuser", middleware.CreateUser).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/user/{id}", middleware.UpdateUser).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deleteuser/{id}", middleware.DeleteUser).Methods("DELETE", "OPTIONS")
+
+	return router
+}
