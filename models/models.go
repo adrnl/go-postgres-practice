9f7f98be7bb8761e32110c6dcfe056412a054533
Adrian <adrian.lu95@gmail.com>
2021-02-01 16:29:50 -0800

    initializing new product type

--- models/models.go
+++ models/models.go
@@ -7,3 +7,10 @@ type User struct {
 	Location string `json:"location"`
 	Age      int64  `json:"age"`
 }
+
+// Product Schema
+type Product struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+	MSRP int64  `json:"msrp"`
+}
